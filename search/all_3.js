5b5e4da60ff93319e3cd21d888eb333976877344
Travis CI <support@travis-ci.org>
2019-05-13 19:56:47 +0000

    Automated documentation update

--- search/all_3.js
+++ search/all_3.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['github_5fdoc_5fcommit',['github_doc_commit',['../cd-ci-glue_8bash.html#a85c61403f600caead4e65d0cadef072a',1,'cd-ci-glue.bash']]],
   ['github_5fpages_5fprepare',['github_pages_prepare',['../cd-ci-glue_8bash.html#a49d4adba8cbc868de2f7e727fd6cb178',1,'cd-ci-glue.bash']]],
-  ['github_5freleases_5fget_5flatest',['github_releases_get_latest',['../cd-ci-glue_8bash.html#ace3bc7cdce98422d7ed66ebb168b9ad0',1,'cd-ci-glue.bash']]],
+  ['github_5freleases_5fget_5flatest',['github_releases_get_latest',['../cd-ci-glue_8bash.html#aeffa75213cea0287ceac773fc8b888cf',1,'cd-ci-glue.bash']]],
   ['github_5fwiki_5fcommit',['github_wiki_commit',['../cd-ci-glue_8bash.html#a568b5c1980de49f8ca9cca1d271d2344',1,'cd-ci-glue.bash']]],
   ['github_5fwiki_5fprepare',['github_wiki_prepare',['../cd-ci-glue_8bash.html#a635535913bb417dc82541522258f1a95',1,'cd-ci-glue.bash']]]
 ];
