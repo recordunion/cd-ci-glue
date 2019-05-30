19c6e82350bc4042edc6026c0fc3db72502ec6e1
Travis CI <support@travis-ci.org>
2019-05-30 14:33:02 +0000

    Automated documentation update

--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['is_5ftravis_5fbranch_5fpush',['is_travis_branch_push',['../cd-ci-glue_8bash.html#ad017448657e3c20940e5c30cadd02804',1,'cd-ci-glue.bash']]],
+  ['is_5ftravis_5fcron',['is_travis_cron',['../cd-ci-glue_8bash.html#a0950b16c99604429c5ec2011caf05f8f',1,'cd-ci-glue.bash']]],
   ['is_5ftravis_5fmaster_5fpush',['is_travis_master_push',['../cd-ci-glue_8bash.html#a327598396a1b40c158f4c83c104dde9d',1,'cd-ci-glue.bash']]]
 ];
